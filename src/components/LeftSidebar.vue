42b012a2f10887d31ba1b64af2979e801c789b23
nikrussia <kkrhrcdc@gmail.com>
2022-08-04 11:52:22 +0300

    fix

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="list">
+    <h3>
+      Фильтрация по amount
+    </h3>
     <label><input value="" type="radio" v-model="changeFilter"> Все</label>
     <label><input value="less" type="radio" v-model="changeFilter"> Меньше 0</label>
     <label><input value="more" type="radio" v-model="changeFilter"> Больше или равно 0 </label>
     <hr>
 
+    <h3>
+      Сортировка по amount
+    </h3>
     <label><input type="checkbox" :checked="true" v-model="changeSort"> Сортировать по сумме</label>
     <hr>
 
+    <h3>
+      Кол-во сгенерированных итемов
+    </h3>
     <label><input type="number" v-model="arrayLength"></label>
     <label><input type="checkbox" v-model="isLocalDisabled">Заблокировать</label>
   </div>
@@ -45,5 +54,9 @@ export default {
 };
 </script>
 <style lang="scss">
-
+.list > h3 {
+  padding: 0 calc(0.5em / 1.2);
+  font-weight: bold !important;
+  font-size: 1.2em !important;
+}
 </style>
